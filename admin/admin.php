a3955c72c5a0bb4d0a20e7f872a582602a9b1b42
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-16 13:20:10 +0300

    Update admin.php

---
+++ admin/admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" media="(max-width: 768px)" href="adminmobile.css">
+    <link rel="stylesheet" media="(max-width: 500px)" href="admintablet.css">
+</head>
+<body>
+    
+</body>
+</html>
