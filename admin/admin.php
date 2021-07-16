bd98b94583d91c0aa57ee7d9f4afde29e00fa411
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-16 13:25:50 +0300

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="admin.css">
-    <link rel="stylesheet" media="(max-width: 768px)" href="adminmobile.css">
-    <link rel="stylesheet" media="(max-width: 500px)" href="admintablet.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" media="(max-width: 768px)" href="css/adminmobile.css">
+    <link rel="stylesheet" media="(max-width: 500px)" href="css/admintablet.css">
 </head>
 <body>
     <header>
