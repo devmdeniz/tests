c454a68e2f2ce920165384cf29541ae1076810a7
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-17 14:43:37 +0300

    Create connection.php

---
+++ admin/database/connection.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$database = "test";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn) {
+    die("Database Err");
+}
+
+?>
