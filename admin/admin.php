b12e966f6051f39866fb793fa0a8bcc858087a93
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-17 19:49:41 +0300

    Update forms from admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -39,141 +39,87 @@ $formlink = "database/adminforms.php";
         <div>
             <h1>Image 1</h1>
                 <input type="url" name="presentationthree" required>
-            </form>
         </div>
     </section>
 <!--  -->
     <section id="portfolio">
         <div>
             <h1>Image 1</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfolioone" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Paragraph 1</h1>
-            <form action=<?php echo $formlink;?>>
             <input type="text" name="portfoliotwo" required>
-            <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 1</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfoliothree" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Image 2</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfoliofour" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Paragraph 2</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="text" name="portfoliofive" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 2</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfoliosix" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Image 3</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfolioseven" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Paragraph 3</h1>
-            <form action=<?php echo $formlink;?>>
             <input type="text" name="portfolioeight" required>
-            <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 3</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="portfolionine" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
     </section>
 
     <section id="blog">
         <div>
             <h1>Image 1</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="blogone" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 1</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="blogtwo" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Image 2</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="blogthree" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 2</h1>
-            <form action=<?php echo $formlink;?>>
             <input type="url" name="blogfour" required>
-                <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Image 2</h1>
-            <form action=<?php echo $formlink;?>>
             <input type="url" name="blogfive" required>
-            <button type="submit">Send</button>
-            </form>
         </div>
         <div>
             <h1>Link 3</h1>
-            <form action=<?php echo $formlink;?>>
             <input type="url" name="blogsix" required>
-            <button type="submit">Send</button>
-            </form>
         </div>
     </section>
 
     <section id="contact">
     <div>
             <h1>Mail</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="email" name="contactone" required>
-                <button type="submit">Send</button>
-            </form>
     </div>
     <div>
             <h1>LinedIn</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="contacttwo" required>
-                <button type="submit">Send</button>
-            </form>
     </div>
     <div>
             <h1>Github</h1>
-            <form action=<?php echo $formlink;?>>
                 <input type="url" name="contactthree" required>
-                <button type="submit">Send</button>
             </form>
     </div>
     </section>
