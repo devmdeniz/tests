f5e2f0c7d5f47c98b2cb2bd3e9b2efaf566985cc
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-17 18:48:37 +0300

    Update adminforms.php

--- admin/database/adminforms.php
+++ admin/database/adminforms.php
@@ -1,2 +1,132 @@
 <?php
+
+// Presentation = 3
+// Portfolio = 9
+// Blog = 6
+// Contact = 3
+
+// 
+if(isset($_GET["presentationone"])) {
+    $presentationone = $_GET["presentationone"];
+}
+
+if(isset($_GET["presentationtwo"])) {
+    $presentationtwo = $_GET["presentationtwo"];
+}
+
+if(isset($_GET["presentationthree"])) {
+    $presentationthree = $_GET["presentationthree"];
+}
+
+// 
+// 
+// 
+
+if(isset($_GET["portfolioone"])) {
+    $portfolioone = $_GET["portfolioone"];
+}
+
+if(isset($_GET["portfoliotwo"])) {
+    $portfoliotwo = $_GET["portfoliotwo"];
+}
+
+if(isset($_GET["portfoliothree"])) {
+    $portfoliothree = $_GET["portfoliothree"];
+}
+
+if(isset($_GET["portfoliofour"])) {
+    $portfoliofour = $_GET["portfoliofour"];
+}
+
+if(isset($_GET["portfoliofive"])) {
+    $portfoliofive = $_GET["portfoliofive"];
+}
+
+if(isset($_GET["portfoliosix"])) {
+    $portfoliosix = $_GET["portfoliosix"];
+}
+
+if(isset($_GET["portfolioseven"])) {
+    $portfolioseven = $_GET["portfolioseven"];
+}
+
+if(isset($_GET["portfolioeight"])) {
+    $portfolioeight = $_GET["portfolioeight"];
+}
+
+if(isset($_GET["portfolionine"])) {
+    $portfolionine = $_GET["portfolionine"];
+}
+// 
+// 
+// 
+
+if(isset($_GET["blogone"])) {
+    $blogone = $_GET["blogone"];
+}
+
+if(isset($_GET["blogtwo"])) {
+    $blogtwo = $_GET["blogtwo"];
+}
+
+if(isset($_GET["blogthree"])) {
+    $blogthree = $_GET["blogthree"];
+}
+
+if(isset($_GET["blogfour"])) {
+    $blogfour = $_GET["blogfour"];
+}
+
+if(isset($_GET["blogfive"])) {
+    $blogfive = $_GET["blogfive"];
+}
+
+if(isset($_GET["blogsix"])) {
+    $blogsix = $_GET["blogsix"];
+}
+// 
+// 
+// 
+
+if(isset($_GET["contactone"])) {
+    $contactone = $_GET["contactone"];
+}
+
+if(isset($_GET["contacttwo"])) {
+    $contacttwo = $_GET["contacttwo"];
+}
+
+if(isset($_GET["contactthree"])) {
+    $contactthree = $_GET["contactthree"];
+}
+
+/*
+$presentationthree = $_GET["presentationthree"];
+// 
+$portfolioone = $_GET["portfolioone"];
+$portfoliotwo = $_GET["portfoliotwo"];
+$portfoliothree = $_GET["portfoliothree"];
+$portfoliofour = $_GET["portfoliofour"];
+$portfoliofive = $_GET["portfoliofive"];
+$portfoliosix = $_GET["portfoliosix"];
+$portfolioseven = $_GET["portfolioseven"];
+$portfolioeight = $_GET["portfolioeight"];
+$portfolionine = $_GET["portfolionine"];
+// 
+$blogone = $_GET["blogone"];
+$blogtwo = $_GET["blogtwo"];
+$blogthree = $_GET["blogthree"];
+$blogfour = $_GET["blogfour"];
+$blogfive = $_GET["blogfive"];
+$blogsix = $_GET["blogsix"];
+// 
+$contactone = $_GET["contactone"];
+$contacttwo = $_GET["contacttwo"];
+$contactthree = $_GET["contactthree"];
+// 
+*/
+if(isset($presentationone)) {
+    die("oldu la");
+}
+
 ?>
