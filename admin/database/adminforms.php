2fe4e12852ace0ef2b036dd5cfc2ce032390b78f
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-07-19 16:50:11 +0300

    Update adminforms.php

--- admin/database/adminforms.php
+++ admin/database/adminforms.php
@@ -16,16 +16,29 @@ global $conn;
     }
 }
 
+function updateMail($mail, $id) {
+    global $conn;
+    $text = 'mailto:'. $mail;
+    if($sql = mysqli_query($conn, 'UPDATE `index` SET `text` = $text WHERE `index`.`id` = $id')) {
+        echo "Succesfull";
+    } else {
+        die('err' . $id . $text . mysqli_error($conn));
+    }
+}
+
 if(isset($_GET["presentationone"])) {
     $presentationone = $_GET["presentationone"];
+    updateData($presentationone, 1);
 }
 
 if(isset($_GET["presentationtwo"])) {
     $presentationtwo = $_GET["presentationtwo"];
+    updateData($presentationtwo, 2);
 }
 
 if(isset($_GET["presentationthree"])) {
     $presentationthree = $_GET["presentationthree"];
+    updateData($presentationthree, 3);
 }
 
 // 
@@ -34,38 +47,47 @@ if(isset($_GET["presentationthree"])) {
 
 if(isset($_GET["portfolioone"])) {
     $portfolioone = $_GET["portfolioone"];
+    updateData($portfolioone, 5);
 }
 
 if(isset($_GET["portfoliotwo"])) {
     $portfoliotwo = $_GET["portfoliotwo"];
+    updateData($portfoliotwo, 6);
 }
 
 if(isset($_GET["portfoliothree"])) {
     $portfoliothree = $_GET["portfoliothree"];
+    updateData($portfoliothree, 4);
 }
 
 if(isset($_GET["portfoliofour"])) {
     $portfoliofour = $_GET["portfoliofour"];
+    updateData($portfoliofour, 8);
 }
 
 if(isset($_GET["portfoliofive"])) {
     $portfoliofive = $_GET["portfoliofive"];
+    updateData($portfoliofive, 9);
 }
 
 if(isset($_GET["portfoliosix"])) {
     $portfoliosix = $_GET["portfoliosix"];
+    updateData($portfoliosix, 7);
 }
 
 if(isset($_GET["portfolioseven"])) {
     $portfolioseven = $_GET["portfolioseven"];
+    updateData($portfolioseven, 11);
 }
 
 if(isset($_GET["portfolioeight"])) {
     $portfolioeight = $_GET["portfolioeight"];
+    updateData($portfolioeight, 12);
 }
 
 if(isset($_GET["portfolionine"])) {
     $portfolionine = $_GET["portfolionine"];
+    updateData($portfolionine, 10);
 }
 // 
 // 
@@ -73,26 +95,32 @@ if(isset($_GET["portfolionine"])) {
 
 if(isset($_GET["blogone"])) {
     $blogone = $_GET["blogone"];
+    updateData($blogone, 14);
 }
 
 if(isset($_GET["blogtwo"])) {
     $blogtwo = $_GET["blogtwo"];
+    updateData($blogtwo, 13);
 }
 
 if(isset($_GET["blogthree"])) {
     $blogthree = $_GET["blogthree"];
+    updateData($blogthree,16);
 }
 
 if(isset($_GET["blogfour"])) {
     $blogfour = $_GET["blogfour"];
+    updateData($blogfour, 15);
 }
 
 if(isset($_GET["blogfive"])) {
     $blogfive = $_GET["blogfive"];
+    updateData($blogfive, 18);
 }
 
 if(isset($_GET["blogsix"])) {
     $blogsix = $_GET["blogsix"];
+    updateData($blogsix, 17);
 }
 // 
 // 
@@ -100,14 +128,17 @@ if(isset($_GET["blogsix"])) {
 
 if(isset($_GET["contactone"])) {
     $contactone = $_GET["contactone"];
+    updateMail($contactone, 19);
 }
 
 if(isset($_GET["contacttwo"])) {
     $contacttwo = $_GET["contacttwo"];
+    updateData($text, 20);
 }
 
 if(isset($_GET["contactthree"])) {
     $contactthree = $_GET["contactthree"];
+    updateData($text, 21);
 }
 
 /*
